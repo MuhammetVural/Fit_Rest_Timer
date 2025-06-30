7d7146678cacd6792cad1265816c033593eff7e5
Muhammet Vural <muhammetvural@anadolu.edu.tr>
2025-06-30 10:52:45 +0300

    last commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.vural.fitresttimer"
         minSdk = 33
         targetSdk = 34
-        versionCode = 1
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
